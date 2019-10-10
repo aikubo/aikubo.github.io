10c55817134715bf7f0c61f9a2e8cc457006acf7
khutchison <kirkhutchison@gmail.com>
2019-10-10 14:59:55 -0700

    add photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import { Link, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
+import Img from 'gatsby-image'
 
 const IndexPage = () => {
+    const data = useStaticQuery(`
+    query MyQuery {
+        image: file(relativePath: {eq: "images/allie.JPG"}) {
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
+      }`)
+
     return (
         <Layout>
+            <Img fluid={data.image.childImageSharp.fluid}>
+            </Img>
             <h1>Hello.</h1>
             <h2>I'm Allison, a PhD student living in beautiful Eugene Oregon.</h2>
             <p>Need a rock doc? <Link to="/contact">Contact Me.</Link></p>
