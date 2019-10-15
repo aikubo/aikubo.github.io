c6dd13073161c31e922893ff20ba271885bf1c8c
khutchison <kirkhutchison@gmail.com>
2019-10-14 17:01:11 -0700

    Remove double header bug log page

--- src/templates/log.js
+++ src/templates/log.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Sidebar from '../components/sidebar'
 import Layout from '../components/layout'
 
 export const query = graphql`
@@ -18,7 +17,6 @@ export const query = graphql`
 const Log = (props) => {
     return (
         <Layout>
-            <Sidebar />
             <h1>{props.data.markdownRemark.frontmatter.title}</h1>
             <p>{props.data.markdownRemark.frontmatter.date}</p>
             <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
