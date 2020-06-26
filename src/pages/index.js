f7c100a28c9312c6f7b7ce78f4d26ab18c7c28a7
aikubo <alli.i.kubo@gmail.com>
2020-06-26 01:14:57 -0700

    Fixed home page padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,11 +23,11 @@ const IndexPage = () => {
                 <div className={indexStyles.picture}><Img fluid={data.image.childImageSharp.fluid} alt="A picture of Allison." /></div>
                 <div className={indexStyles.content}>
                   <p className={indexStyles.tagline}> <i>I use computational models to explore fluids produced by volcanoes. </i> </p>
-                  <p>PhD Student at the University of Oregon</p>
+                  <p>Ph.D. Candidate at the University of Oregon</p>
                   <p>Advised by <a href="https://pages.uoregon.edu/jdufek/" target="_blank" rel="noopener noreferrer">Joe Dufek</a> and <a href="https://pages.uoregon.edu/leif/" target="_blank" rel="noopener noreferrer">Leif Karlstrom</a></p>
                   <p className={indexStyles.github}>Check out my work on <a href="https://github.com/aikubo" target="_blank" rel="noopener noreferrer">Github</a>.</p>
                   
-                  <p><a href="mailto:akubo@uoregon.edu">akubo@uoregon.edu </a> </p>
+                  <p className={indexStyles.email}><a href="mailto:akubo@uoregon.edu">akubo@uoregon.edu </a> </p>
 
                   
                   
